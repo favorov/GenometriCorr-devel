4a092a9ade32be13ec8102455aef5d309568bb8a
Alexander Favorov <favorov@sensi.org>
2020-01-31 02:39:14 -0500

    RangedData is on removal removed from the main GenometriCorrelation; thinking about seqlenghts

--- pkg/R/GenometriCorrelation.R
+++ pkg/R/GenometriCorrelation.R
@@ -498,13 +498,15 @@ GenometriCorrelation <- function(
 			}
 		}
 		
-		if (cut.all.over.length)
-		{
-			que_ranges<-que_ranges %>% transform(end,pmin(end,chromosomes.length[space]))
-			ref_ranges<-ref_ranges %>% transform(end,pmin(end,chromosomes.length[space]))
-		}
 	
 	}
+	
+	#we cannot create a GRanges with a seqlength[space] < max(ends(gr %>% filter(seqlength==space))
+	#if (cut.all.over.length)
+	#{
+	#	que_ranges<-que_ranges %>% transform(end,pmin(end,chromosomes.length[space]))
+	#	ref_ranges<-ref_ranges %>% transform(end,pmin(end,chromosomes.length[space]))
+	#}
 	
 	#the code romoves all the chromosomes with empty r and empty q
 
